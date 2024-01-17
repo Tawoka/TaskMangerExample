6499b0b70ec6459876d48cda98c35f288a7067b9
Rene Morgner <github@rmorgner.com>
2024-01-17 10:20:37 +0100

    initializing Spring Boot 3 Project

---
+++ src/main/java/com/rmorgner/taskmanager/TaskManagerExampleApplication.java
@@ -0,0 +1,13 @@
+package com.rmorgner.taskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagerExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rmorgner/taskmanager/TaskManagerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmorgner.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
